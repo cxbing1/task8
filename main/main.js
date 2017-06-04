2a4dcb33089a3fcb113d09f449725906dd6a34d9
cxbing1 <983121062@qq.com>
2017-06-04 18:18:54 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,40 @@
-module.exports = function main() {
+module.exports = function main(s) {
     console.log("Debug Info");
-    return 'Hello World!';
+    var number=
+	["._.\n|.|\n|_|",  
+
+	"...\n..|\n..|",  
+
+	"._.\n._|\n|_.",  
+
+	"._.\n._|\n._|",  
+
+	"...\n|_|\n..|",  
+
+	"._.\n|_.\n._|",  
+
+	"._.\n|_.\n|_|", 
+
+	"._.\n..|\n..|",  
+
+	"._.\n|_|\n|_|",  
+
+	"._.\n|_|\n..|"]
+
+
+var text="";
+for(var i=0;i<3;i++)
+for(var j=0;j<s.length;j++)
+{
+    var t=number[s[j]-0].split("\n");
+    text+=t[i];
+    if(j<s.length-1)
+    text+=" ";
+    else
+    text+="\n";
+
+}//+number[s[1]-0].split("\n")[0]+" "number[s[2]-0].split("\n")[0]+"\n"
+
+
+    return text;
 };
